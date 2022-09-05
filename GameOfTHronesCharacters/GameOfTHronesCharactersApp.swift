f516ee6ed24a7ea3dcf8135ae11e510272d41c06
Igor Gadomskii <igvgad@gmail.com>
2022-09-05 18:27:00 +0300

    Initial Commit

---
+++ GameOfTHronesCharacters/GameOfTHronesCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfTHronesCharactersApp.swift
+//  GameOfTHronesCharacters
+//
+//  Created by Macbook on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameOfTHronesCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
